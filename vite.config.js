cc956e85f4df9b1a2737e7f43227d12e8571908b
황정현 <wjdgus982@naver.com>
2026-08-05 15:08:20 +0900

    fix: config 수정

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/skala-vue/' : '/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
